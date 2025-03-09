6cf546a7e8922a4be56144d9cc16d25369269922
Yannick Battail <yannick.battail@gmail.com>
2025-03-09 18:42:52 +0100

    reformat

--- srv/server.ts
+++ srv/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { handlePreview } from "./handlers/handlePreview.js";
 import { handleAnimation } from "./handlers/handleAnimation.js";
+import { handleParameter } from "./handlers/handleParameter.js";
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.post("/preview", handlePreview);
+app.post("/parameter", handleParameter);
 app.post("/animation", handleAnimation);
 
 app.listen(port, () => {
